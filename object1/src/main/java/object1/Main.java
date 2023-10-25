850b0b4411d2b6017ba6fb47258a61f4389ac2c4
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-10-25 14:51:50 -0400

    finished, added log for height +  added array for dogs.

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -8,8 +8,8 @@ public class Main {
         Logger logger = Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");
 
-        Person[] people = new Person[5];
         Person person1 = new Person("Tam", 24);
+        logger.info("New Person created. Height is " + person1.getHeight());
         Person person2 = new Person("Maria", 34);
         Person person3 = new Person("Alex", 45);
         Person person4 = new Person("Barbara", 56);
@@ -17,7 +17,11 @@ public class Main {
 
         Chair chair1 = new Chair("Wood", "Brown", 4, 80);
         Dog dog1 = new Dog("Dasi", 8, "Chihuahua");
+        Dog dog2 = new Dog("Imaginary dog", 5, "Weiner");
         Car car1 = new Car("Honda", "Accord", 2013, 96000, 12);
+
+        Person[] people = new Person[5];
+        Dog[] dogs = new Dog[2];
 
         people[0] = person1;
         people[1] = person2;
@@ -25,14 +29,18 @@ public class Main {
         people[3] = person4;
         people[4] = person5;
 
+        dogs[0] = dog1;
+        dogs[1] = dog2;
+
         for(int i = 0; i < people.length; i++){
             System.out.println("Hello, my name is " + people[i].name + ". I'm " + people[i].age + " years old.");
             if(people[i].name.equals("Tam")){
-                System.out.println("I own a " + car1.getMake() + " " + car1.getModel() + "! I also have a dog named " + dog1.getName() + "!");
+                System.out.println("I own a " + car1.getMake() + " " + car1.getModel() + "! I also have a dog named " + dogs[0].getName() + "!");
+            }
+            else{
+                System.out.println("I own a dog named " + dogs[1].getName() + ".");
             }
         }
-
-        System.out.println("Testing getter for height: " + person1.getHeight());
 
     }
 }
